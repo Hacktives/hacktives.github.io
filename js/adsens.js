210d0baa50aff6dd220a093abc1291f3596d0482
hacktives <55632905+Hacktives@users.noreply.github.com>
2023-11-26 16:08:08 +0700

    Update adsens.js

--- js/adsens.js
+++ js/adsens.js
@@ -1,2 +1,3 @@
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2399699387976173"
      crossorigin="anonymous"></script>
+<meta name="google-adsense-account" content="ca-pub-2399699387976173">
