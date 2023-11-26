71e66a68338527f4a2c8d4d86a45e43625122acd
hacktives <55632905+Hacktives@users.noreply.github.com>
2023-11-26 16:05:57 +0700

    Update GA.js

--- js/GA.js
+++ js/GA.js
@@ -1,2 +1,9 @@
-<script src="/js/adsens.js"></script>
-    <script src="/js/GA.js"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KDD2FND9CF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KDD2FND9CF');
+</script>
